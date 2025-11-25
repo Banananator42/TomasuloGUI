e14da8ee8610ccc9c5558037302e2a1ce4cc67cc
happytuna345 <ajankowski@cedarville.edu>
2025-11-25 16:45:06 -0500

    Added initial Branch Logic

--- src/tomasulogui/BranchUnit.java
+++ src/tomasulogui/BranchUnit.java
@@ -14,20 +14,9 @@ public class BranchUnit
         int result = 0;
         int data1 = stations[station].getData1();
         int data2 = stations[station].getData2();
-        //No switch statement here, all the calculation needs to do is compare two numbers
-        result = data1 - data2;
-        // switch (stations[station].getFunction()) {
-        //     case BEQ -> result = data1 - data2;
-        //     case BGEZ -> result = data1 - data2;
-        //     case BLEZ -> result = data1 & data2;
-        //     case BNE -> result = data1 | data2;
-        //     case BGTZ -> result = data1 ^ data2;
-        //     case BLTZ -> result = data1 << data2;
-        //     case JAL -> result = data1 >>> data2;
-        //     case JALR -> result = data1 >> data2;
-        //     case J -> result = data1 >>> data2;
-        //     case JR -> result = data1 >> data2;
-        // }
+
+        result = data1 - data2; //might need to do data2 - data1
+
         return result;
     }
 
--- src/tomasulogui/FunctionalUnit.java
+++ src/tomasulogui/FunctionalUnit.java
@@ -1,41 +1,64 @@
 package tomasulogui;
 
 public abstract class FunctionalUnit {
-  PipelineSimulator simulator;
-  ReservationStation[] stations = new ReservationStation[2];
+    PipelineSimulator simulator;
+    ReservationStation[] stations = new ReservationStation[2];
 
     int writeBackVal = -1;
     int writeBackTag = -1;
     boolean requestWriteback = false;
     int writeBackStation = -1;
-  
-  public FunctionalUnit(PipelineSimulator sim) {
-    simulator = sim;
-  }
 
- 
-  public void squashAll() {
-    for (int i = 0; i < stations.length; i++) {
-        stations[i].isSquashed = true;
+    public FunctionalUnit(PipelineSimulator sim) {
+        simulator = sim;
     }
-  }
 
-  public abstract int calculateResult(int station);
 
-  public abstract int getExecCycles();
+    public void squashAll() {
+        for (int i = 0; i < stations.length; i++) {
+            stations[i].isSquashed = true;
+        }
+    }
 
-  public void execCycle(CDB cdb) {
-    //todo - start executing, ask for CDB, etc.
-    //PROBLEM should we always compute the first reservation station?
-    for (int i = 0; i < 2; i++) {
-      if (stations[i] != null && stations[i].data1Valid && stations[i].data2Valid) {
-          writeBackVal = calculateResult(i);
-          requestWriteback = true;
-          writeBackTag = stations[i].destTag;
-          writeBackStation = i;
-      }
-    } 
-  }
+    public abstract int calculateResult(int station);
+
+    public abstract int getExecCycles();
+
+    public void execCycle(CDB cdb) {
+        //todo - start executing, ask for CDB, etc.
+        //PROBLEM should we always compute the first reservation station?
+        for (int i = 0; i < 2; i++) {
+            if (stations[i] != null && stations[i].data1Valid && stations[i].data2Valid) {
+                if(this instanceof IntAlu) {
+                    writeBackVal = calculateResult(i);
+                    requestWriteback = true;
+                    writeBackTag = stations[i].destTag;
+                    writeBackStation = i;
+                }
+                else if(this instanceof BranchUnit) {
+                    int branchCompare = calculateResult(i);
+                    boolean isBranchTaken = false; //might need to exist elsewhere, added here for now
+                    switch (stations[i].getFunction()) {
+                        case BEQ -> isBranchTaken = branchCompare == 0;
+                        case BGEZ -> isBranchTaken = branchCompare >= 0;
+                        case BLEZ -> isBranchTaken = branchCompare <= 0;
+                        case BNE -> isBranchTaken = branchCompare != 0;
+                        case BGTZ -> isBranchTaken = branchCompare > 0;
+                        case BLTZ -> isBranchTaken = branchCompare < 0;
+                        //need to implement J functions
+                        //NEED TO set MISS button false in ROB if getBranchPrediction()
+                        case JAL, JALR, J, JR -> isBranchTaken = true;
+                    }
+                    if(isBranchTaken) {
+                        requestWriteback = true;
+                        writeBackTag = stations[i].destTag;
+                        writeBackStation = i;
+                    }
+                    simulator.getBTB().setBranchResult(simulator.getPC(), isBranchTaken); //train the BTB with branch data?
+                }
+            }
+        }
+    }
 
     public boolean isReservationStationAvail() { //modeled after method in LoadBuffer
         for (int i=0; i < stations.length; i++) {
@@ -46,21 +69,21 @@ public abstract class FunctionalUnit {
         return false;
     }
 
-  public void acceptIssue(IssuedInst inst) {
-  // todo - fill in reservation station (if available) with data from inst
-      //Pretty much copied this over from LoadBuffer
-      int slot=0;
-      for (slot=0; slot < 2; slot++) {
-          if (stations[slot] == null) {
-              break;
-          }
-      }
-      if (slot == 2) {
-          throw new MIPSException("Loader accept issue: slot not available");
-      }
+    public void acceptIssue(IssuedInst inst) {
+        // todo - fill in reservation station (if available) with data from inst
+        //Pretty much copied this over from LoadBuffer
+        int slot=0;
+        for (slot=0; slot < 2; slot++) {
+            if (stations[slot] == null) {
+                break;
+            }
+        }
+        if (slot == 2) {
+            throw new MIPSException("Loader accept issue: slot not available");
+        }
 
-      ReservationStation entry = new ReservationStation(simulator);
-      stations[slot] = entry;
-      entry.loadInst(inst);
-      }
-  }
+        ReservationStation entry = new ReservationStation(simulator);
+        stations[slot] = entry;
+        entry.loadInst(inst);
+    }
+}
